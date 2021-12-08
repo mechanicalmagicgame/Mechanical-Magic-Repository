c2546f51a34a574bfd305dc7bec8dc89f3da90a1
Prathmesh Jain <prathmeshjain02@gmail.com>
2021-12-08 12:41:24 -0800

    Better Camera Follow
    - Implemented a better camera follow script
    - The player can now move freely in a small area within the camera FOV before the camera starts to follow it
    - Gives a better idea of the player
    - Makes movement a bit more smooth
    - The camera follow motion is also smoother now

---
+++ Mechanical Magic/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float xDiff, yDiff;
+    [Range(1,100)]
+    public float smoothFactor;
+
+    private float targetXPos, targetYPos, camXPos, camYPos;
+
+    private void FixedUpdate()
+    {
+        Follow();
+    }
+
+    void Follow()
+    {
+        camXPos = transform.position.x;
+        camYPos = transform.position.y;
+        targetXPos = target.position.x;
+        targetYPos = target.position.y;
+        
+
+        if((Mathf.Abs(camXPos - targetXPos) > xDiff) || (Mathf.Abs(camYPos - targetYPos) > yDiff))
+        {
+            Vector3 targetPosition = target.position + offset;
+            Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, (smoothFactor / 10) * Time.fixedDeltaTime);
+            transform.position = smoothPosition;
+        }
+    }
+}
